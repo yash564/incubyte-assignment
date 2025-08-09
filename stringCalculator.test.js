3ec4df91cc71ae76c750c9af5aa1163553dd8b05
yash564 <yashk2700@gmail.com>
2025-08-09 13:26:23 +0530

    One Number Test Case

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -4,5 +4,9 @@ describe('String Calculator', () => {
     test('empty string returns 0', () => {
         expect(add("")).toBe(0);
     });
+
+    test('single number returns its value', () => {
+        expect(add("1")).toBe(1);
+    });
 })
 
